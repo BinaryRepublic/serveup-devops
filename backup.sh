f6975bcca5bd9394e32a1ec64baccaddfe7b70fe
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-06 12:20:27 +0100

    Added start-Scripts

--- backup.sh
+++ backup.sh
@@ -1,2 +1,5 @@
-gsutil cp -r /var/lib/docker/volumes/dev_realm-data/_data gs://ro-realm-backup/dev
-gsutil cp -r /var/lib/docker/volumes/prod_realm-data/_data gs://ro-realm-backup/prod
+gsutil cp -r /var/lib/docker/volumes/dev_realm-data/_data/default.realm gs://ro-realm-backup/dev/default.realm
+gsutil cp -r /var/lib/docker/volumes/dev_realm-data/_data/default.realm.lock gs://ro-realm-backup/dev/default.realm
+
+gsutil cp -r /var/lib/docker/volumes/prod_realm-data/_data/default.realm gs://ro-realm-backup/prod/default.realm
+gsutil cp -r /var/lib/docker/volumes/prod_realm-data/_data/default.realm.lock gs://ro-realm-backup/prod/default.realm
---
+++ dev/start.sh
@@ -0,0 +1,4 @@
+sleep 10
+docker-compose down
+docker-compose pull
+docker-compose up -d
