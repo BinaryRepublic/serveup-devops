79ceda573643e39fc4c59340208a0c6d7d505f6d
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-06 17:03:33 +0100

    Updated start scripts

--- prod/start.sh
+++ prod/start.sh
@@ -1,5 +1,6 @@
 sleep 10
+export PATH=/home/ro/bin:/home/ro/.local/bin:/home/ro/gcloud/google-cloud-sdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 docker-compose down
-docker rmi "gcr.io/restaurant-order-192310/$1"
+docker rmi gcr.io/restaurant-order-192310/$1
 docker-compose pull
 docker-compose up -d
