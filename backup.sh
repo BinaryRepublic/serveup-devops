0bbd4dd9652790400c9884f34e75902e1fdbf6cc
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-14 18:12:30 +0100

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
-timestamp=$(date +"%d_%m_%Y %H:%M")
+timestamp=$(date +"%d-%m-%Y_%H:%M")
 
-
-gsutil cp -r /var/lib/docker/volumes/dev_realm-data/_data/default.realm "gs://ro-realm-backup/dev/$timestamp_default.realm"
-gsutil cp -r /var/lib/docker/volumes/prod_realm-data/_data/default.realm "gs://ro-realm-backup/prod/$timestamp_default.realm"
+gsutil cp -r /var/lib/docker/volumes/dev_realm-data/_data/default.realm "gs://ro-realm-backup/dev/$timestamp.default.realm"
+gsutil cp -r /var/lib/docker/volumes/prod_realm-data/_data/default.realm "gs://ro-realm-backup/prod/$timestamp.default.realm"
