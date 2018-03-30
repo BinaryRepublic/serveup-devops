2d6e58645bd8b1ca2c4bb8c623bfc8890d736607
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-30 15:41:06 +0200

    Added scripts

---
+++ down.sh
@@ -0,0 +1,7 @@
+docker-compose down
+cd /openapi
+docker-compose down
+cd ../dev
+docker-compose down
+cd ../prod
+docker-compose down
---
+++ up.sh
@@ -0,0 +1,7 @@
+docker-compose up -d
+cd /openapi
+docker-compose up -d
+cd ../dev
+docker-compose up -d
+cd ../prod
+docker-compose up -d
